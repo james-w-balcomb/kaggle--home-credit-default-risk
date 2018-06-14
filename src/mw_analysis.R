ad47613f701bd374687d3023df7f8ec64f8d8794
Matt Winkler <mwinkler1419@gmail.com>
2018-06-13 19:17:30 -0500

    getting plot functions together for EXP analysis

--- src/mw_analysis.R
+++ src/mw_analysis.R
@@ -32,32 +32,111 @@ train %>% count(TARGET) %>% kable()
 # mean of target:
 mean(train$TARGET)
 
+
+
+
+
 # mean values of variables in tabular format:
 dt <- data.table(train)
 dt[, .(target_avg = mean(train$TARGET))]
 options(scipen = 999) # turn off scientific notation
-dt[, .N / nrow(dt), by = CODE_GENDER]
-dt[, .N / nrow(dt), by = NAME_CONTRACT_TYPE]
-dt[, .N / nrow(dt), by = FLAG_OWN_CAR]
-dt[, .N / nrow(dt), by = FLAG_OWN_REALTY]
-dt[, .N / nrow(dt), by = CNT_CHILDREN]
-dt[, .N / nrow(dt), by = NAME_TYPE_SUITE]
-dt[, .N / nrow(dt), by = NAME_EDUCATION_TYPE]
- # etc . . .
+names <- colnames(dt)
 
-# same idea but with pretty plot:
+class.values <- sapply(dt, class)
+unique(class.values)
+ints <- as.vector(class.values == "integer")
+nums <- as.vector(class.values == "numeric")
+chars <- as.vector(class.values == "character")
+
+sapply(dt[, ..ints], n_unique)
+
+# these columns are probably better treated as numerics:
+dt$DAYS_BIRTH <- as.numeric(dt$DAYS_BIRTH)
+dt$DAYS_BIRTH <- as.numeric(dt$DAYS_EMPLOYED)
+dt$DAYS_BIRTH <- as.numeric(dt$DAYS_ID_PUBLISH)
+
+
+
+
+for (n in names) {
+  # finds the mean value of the target and proportion of the total dataset
+  # represented by level of the factors in the data
+  #result <- dt[, list(mean.val = sum(TARGET) / .N, # avg. value of the target variable by level 
+  #                    prop.records = .N / nrow(dt)), # proportion of records in the data by level
+  #             by = n]
+  
+  print(result)
+}
+
+######## numeric visualizations ########
+
+one_plot <- function(d, col) {
+  plot_ly(d, x = d[, col], type="histogram") %>%
+    add_annotations(
+      ~unique(TARGET), x = 0.5, y = 1, 
+      xref = "paper", yref = "paper", showarrow = FALSE
+    )
+}
+
+
+
+class.values
+
+x.axis <- list(
+  title = c,
+  titlefont = list(
+    family = "Courier New, monospace",
+    size = 18,
+    color = "#7f7f7f")
+)
+
+for (i in 1:length(class.values)) {
+  cv = class.values[[i]]
+  c = names[i]
+  if (cv == "numeric") {
+    
+    plt <- train %>%
+      split(.$TARGET) %>%
+      lapply(one_plot, col=c) %>% 
+      subplot(nrows = 2, shareX = TRUE, titleX = TRUE) %>%
+      layout(xaxis = x.axis) %>%
+      hide_legend()
+    
+    print(plt)
+  }
+}
+
+
+
+######## categorical visualizations ######## 
+
+# proportions of total data by attribute and target variable
 train %>% 
   mutate(TARGET=as.factor(TARGET)) %>% 
   count(NAME_EDUCATION_TYPE, TARGET) %>% 
-  plot_ly(x = ~NAME_EDUCATION_TYPE , y = ~n, color = ~TARGET,type = "bar") %>%
+  mutate(prop = n / nrow(train)) %>%
+  plot_ly(x = ~NAME_EDUCATION_TYPE , y = ~prop, color = ~TARGET,type = "bar") %>%
   # add_trace(y = ~LA_Zoo, name = 'LA Zoo')   %>%
   layout(title = "ORGANIZATION_TYPE Type Group" , 
          barmode = 'stack',
          xaxis = list(title = ""),
          yaxis = list(title = ""))
 
+# proportion of attribute values by target variable:
+dt$TARGET = as.factor(dt$TARGET)
+my.cc <- count(dt, NAME_EDUCATION_TYPE, TARGET)
+my.cc2 <- left_join(my.cc, count(my.cc, NAME_EDUCATION_TYPE, wt = n))
 
-# 
+my.cc2 %>%
+  mutate(prop = n / nn) %>%
+  plot_ly(x = ~NAME_EDUCATION_TYPE, y = ~prop, color = ~TARGET) %>%
+  add_bars() %>%
+  layout(barmode = "stack")
+
+
+
+
+
 
 graph <- list()
 
