c2f7114f86629fb8c910e21ef8a90fb5d008a4f7
James Balcomb <james.w.balcomb@gmail.com>
2018-07-11 20:45:25 -0500

    ready for QA - added testing for Numeric and Ordinal, added handling Pandas Series, added excluded columns

--- jb/ats_get_dummies.py
+++ jb/ats_get_dummies.py
@@ -1,46 +1,95 @@
-#%%
 import pandas as pd
 
-#%%
-data_file_path = 'C:/Development/kaggle--home-credit-default-risk/data/'
-data_file_name = 'application_train.csv'
+from pandas.core.frame import DataFrame
+from pandas.core.series import Series
 
-df = pd.read_table(data_file_path + data_file_name, sep=',')
 
-#%%
-def ats_get_dummies(data, prefix=None, prefix_sep='_', dummy_na=False, columns=None, sparse=False, drop_first=False, dtype=None):
+def ats_get_dummies(data,
+                    prefix=None,
+                    prefix_sep='_',
+                    dummy_na=False,
+                    sparse=False,
+                    included_column_names=None,
+                    excluded_column_names=None):
+    """
+    Generates columns with flags indicating missing values in the existing columns
     
-    ats_dummies_metadata = {}
+    Parameters
+    ----------
+    data : array-like, Series, or DataFrame
+    prefix : string, list of strings, or dict of strings, default None
+    prefix_sep : string, default '_'
+    dummy_na : bool, default False
+    sparse : bool, default False
+    included_column_names : list-like, default None
+    excluded_column_names : list-like, default None
     
-    if not column_names:
-        if df_categorical_column_names:
-            column_names = df_categorical_column_names
+    Returns
+    -------
+    ats_df_dummies : DataFrame
+    """
+    
+    ats_df_dummies = pd.DataFrame()
+    
+    if isinstance(data, DataFrame):
+        
+        if included_column_names is None:
+            if excluded_column_names is not None:
+                columns_to_encode = list(set(data.select_dtypes(include=['object', 'category']).columns) - set(excluded_column_names))
+            else:
+                columns_to_encode = list(set(data.select_dtypes(include=['object', 'category']).columns))
         else:
-            list(df.select_dtypes(include=['category']).columns)
+            if excluded_column_names is not None:
+                columns_to_encode = list(set(included_column_names) - set(excluded_column_names))
+            else:
+                columns_to_encode = list(set(included_column_names))
+        
+        for column_name in columns_to_encode:
+            if(data[column_name].dtype == pd.np.number):
+                columns_to_encode.remove(column_name)
+            elif data[column_name].dtype.name == 'category':
+                if data[column_name].cat.ordered:
+                    columns_to_encode.remove(column_name)
+        
+        for column_name in columns_to_encode:
+            dummy = pd.get_dummies(data[column_name],
+                                   prefix=column_name,
+                                   prefix_sep='__',
+                                   dummy_na=False,
+                                   columns=None,
+                                   sparse=False,
+                                   drop_first=False)
+        
+            ats_df_dummies = pd.concat([ats_df_dummies, dummy], axis=1)
+            
+            # drop the most frequent value for the reference level
+            dropped_column_name = column_name + '__' + data.groupby([column_name]).size().idxmax()
+            ats_df_dummies.drop(dropped_column_name, axis=1, inplace=True)
+        
+        return ats_df_dummies
     
-    for column_name in column_names:
-        df_new_dummies = pd.get_dummies(df[column_name], prefix=column_name, prefix_sep='__')
-        #df_model_data = df_model_data.join(df_new_dummies)
-        df_model_data = pd.concat([df_model_data, df_new_dummies], axis=1)
-        
-        # drop most frequent variable for ref category
-        #dropped_column_name = df.groupby([column_name]).size().idxmax()
-        dropped_column_name = column_name + '__' + df.groupby([column_name]).size().idxmax()
-        df_model_data.drop(dropped_column_name, axis=1, inplace=True)
-        
-        print(column_name + " dropping " + dropped_column_name)
-        print(df.groupby([column_name]).size())
-
-#%%
-df_nonmodel_column_names
-
-#%%
-columns = set(df_dtype_object_column_names) - set(df_nonmodel_column_names)
-columns
-
-
-#%%
-df['combined'] = df['bar'].astype(str) + '_' + df['foo'] + '_' + df['new']
-
-df["combined"] = df["foo"].str.cat(df[["bar", "new"]].astype(str), sep="_")
-
+    elif isinstance(data, Series):
+        if(data.dtype == pd.np.number):
+            return ats_df_dummies
+        elif data.dtype.name == 'category':
+            if data.cat.ordered:
+                return ats_df_dummies
+        else:
+            dummy = pd.get_dummies(data,
+                                   prefix=data.name,
+                                   prefix_sep='__',
+                                   dummy_na=False,
+                                   columns=None,
+                                   sparse=False,
+                                   drop_first=False)
+            
+            ats_df_dummies = pd.concat([ats_df_dummies, dummy], axis=1)
+            
+            # drop the most frequent value for the reference level
+            dropped_column_name = data.name + '__' + data.value_counts().index[0]
+            ats_df_dummies.drop(dropped_column_name, axis=1, inplace=True)
+            
+    else:
+        return ats_df_dummies
+    
+    return ats_df_dummies
