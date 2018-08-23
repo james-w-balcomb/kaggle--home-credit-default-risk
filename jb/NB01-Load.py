6a62dd63291e1874624a37a260a58c3dea61b795
James Balcomb <james.w.balcomb@gmail.com>
2018-08-22 21:30:42 -0500

    added libraries

--- jb/NB01-Load.py
+++ jb/NB01-Load.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import collections
@@ -21,6 +21,7 @@ import seaborn as sns
 #import seaborn as snss
 #import scipy
 import scipy as sp
+import scipy.stats as stats
 import sklearn
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -41,6 +42,8 @@ from pylab import rcParams
 from random import choice
 from random import choices # Python 3.6+
 from random import sample
+from scipy.stats import pointbiserialr
+from scipy.stats import spearmanr
 from sklearn import datasets
 from sklearn import model_selection
 from sklearn import metrics
@@ -90,6 +93,10 @@ matplotlib.style.use('ggplot')
 np.set_printoptions(threshold=np.nan)
 np.set_printoptions(suppress=True, formatter={'float_kind':'{:f}'.format})
 
+# https://pandas.pydata.org/pandas-docs/stable/missing_data.html
+# Note If you want to consider inf and -inf to be “NA” in computations, you can set...
+#pandas.options.mode.use_inf_as_na = True
+
 #import C:/Development/kaggle--home-credit-default-risk/rand_jitter
 #import C:/Development/kaggle--home-credit-default-risk/draw_feature_distribution
 
@@ -106,7 +113,7 @@ from draw_feature_distribution_v2 import *
 # The pandas.core.datetools module is deprecated and will be removed in a future version. Please use the pandas.tseries module instead.
 
 
-# In[2]:
+# In[ ]:
 
 
 print("Python version: {}".format(sys.version))
@@ -118,15 +125,15 @@ print("matplotlib version: {}".format(matplotlib.__version__))
 print("IPython version: {}".format(IPython.__version__))
 
 
-# In[3]:
+# In[ ]:
 
 
-random_seed = 1234567890
-random.seed(random_seed)
-np.random.seed(random_seed)
+RANDOM_SEED = 1234567890
+random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
 
 
-# In[4]:
+# In[ ]:
 
 
 # Set configuration based on environment variables
@@ -144,7 +151,7 @@ else:
 print('data_file_path: ', data_file_path)
 
 
-# In[5]:
+# In[ ]:
 
 
 # Set configuration based on environment variables
@@ -162,7 +169,7 @@ else:
 print('working_directory: ', working_directory)
 
 
-# In[6]:
+# In[ ]:
 
 
 data_file_names = [
@@ -178,7 +185,7 @@ data_file_names = [
 data_file_names
 
 
-# In[7]:
+# In[ ]:
 
 
 df_dataframe_names_and_files = [
@@ -194,7 +201,7 @@ df_dataframe_names_and_files = [
 df_dataframe_names_and_files
 
 
-# In[8]:
+# In[ ]:
 
 
 df_dataset_names_and_files = [
@@ -210,7 +217,7 @@ df_dataset_names_and_files = [
 df_dataset_names_and_files
 
 
-# In[9]:
+# In[ ]:
 
 
 # TODO(JamesBalcomb): add a function that handles specifying multiple files
@@ -218,109 +225,53 @@ data_file_name = 'application_train.csv'
 print('data_file_name: ', data_file_name)
 
 
-# In[10]:
+# In[ ]:
 
 
 df = pd.read_table(
         data_file_path + data_file_name,
         sep=',',
         dtype={
-                'SK_ID_CURR':'object',
-                'TARGET':'object',
-                'NAME_CONTRACT_TYPE':'object',
-                'CODE_GENDER':'object',
-                'FLAG_OWN_CAR':'object',
-                'FLAG_OWN_REALTY':'object',
-                'CNT_CHILDREN':'float64',
-                'AMT_INCOME_TOTAL':'float64',
-                'AMT_CREDIT':'float64',
                 'AMT_ANNUITY':'float64',
+                'AMT_CREDIT':'float64',
                 'AMT_GOODS_PRICE':'float64',
-                'NAME_TYPE_SUITE':'object',
-                'NAME_INCOME_TYPE':'object',
-                'NAME_EDUCATION_TYPE':'object',
-                'NAME_FAMILY_STATUS':'object',
-                'NAME_HOUSING_TYPE':'object',
-                'REGION_POPULATION_RELATIVE':'float64',
+                'AMT_INCOME_TOTAL':'float64',
+                'AMT_REQ_CREDIT_BUREAU_DAY':'float64',
+                'AMT_REQ_CREDIT_BUREAU_HOUR':'float64',
+                'AMT_REQ_CREDIT_BUREAU_MON':'float64',
+                'AMT_REQ_CREDIT_BUREAU_QRT':'float64',
+                'AMT_REQ_CREDIT_BUREAU_WEEK':'float64',
+                'AMT_REQ_CREDIT_BUREAU_YEAR':'float64',
+                'APARTMENTS_AVG':'float64',
+                'APARTMENTS_MEDI':'float64',
+                'APARTMENTS_MODE':'float64',
+                'BASEMENTAREA_AVG':'float64',
+                'BASEMENTAREA_MEDI':'float64',
+                'BASEMENTAREA_MODE':'float64',
+                'CNT_CHILDREN':'float64',
+                'CNT_FAM_MEMBERS':'float64',
+                'CODE_GENDER':'object',
+                'COMMONAREA_AVG':'float64',
+                'COMMONAREA_MEDI':'float64',
+                'COMMONAREA_MODE':'float64',
                 'DAYS_BIRTH':'float64',
                 'DAYS_EMPLOYED':'float64',
-                'DAYS_REGISTRATION':'float64',
                 'DAYS_ID_PUBLISH':'float64',
-                'OWN_CAR_AGE':'float64',
-                'FLAG_MOBIL':'object',
-                'FLAG_EMP_PHONE':'object',
-                'FLAG_WORK_PHONE':'object',
-                'FLAG_CONT_MOBILE':'object',
-                'FLAG_PHONE':'object',
-                'FLAG_EMAIL':'object',
-                'OCCUPATION_TYPE':'object',
-                'CNT_FAM_MEMBERS':'float64',
-                'REGION_RATING_CLIENT':'object',
-                'REGION_RATING_CLIENT_W_CITY':'object',
-                'WEEKDAY_APPR_PROCESS_START':'object',
-                'HOUR_APPR_PROCESS_START':'float64',
-                'REG_REGION_NOT_LIVE_REGION':'object',
-                'REG_REGION_NOT_WORK_REGION':'object',
-                'LIVE_REGION_NOT_WORK_REGION':'object',
-                'REG_CITY_NOT_LIVE_CITY':'object',
-                'REG_CITY_NOT_WORK_CITY':'object',
-                'LIVE_CITY_NOT_WORK_CITY':'object',
-                'ORGANIZATION_TYPE':'object',
+                'DAYS_LAST_PHONE_CHANGE':'float64',
+                'DAYS_REGISTRATION':'float64',
+                'DEF_30_CNT_SOCIAL_CIRCLE':'float64',
+                'DEF_60_CNT_SOCIAL_CIRCLE':'float64',
+                'ELEVATORS_AVG':'float64',
+                'ELEVATORS_MEDI':'float64',
+                'ELEVATORS_MODE':'float64',
+                'EMERGENCYSTATE_MODE':'object',
+                'ENTRANCES_AVG':'float64',
+                'ENTRANCES_MEDI':'float64',
+                'ENTRANCES_MODE':'float64',
                 'EXT_SOURCE_1':'float64',
                 'EXT_SOURCE_2':'float64',
                 'EXT_SOURCE_3':'float64',
-                'APARTMENTS_AVG':'float64',
-                'BASEMENTAREA_AVG':'float64',
-                'YEARS_BEGINEXPLUATATION_AVG':'float64',
-                'YEARS_BUILD_AVG':'float64',
-                'COMMONAREA_AVG':'float64',
-                'ELEVATORS_AVG':'float64',
-                'ENTRANCES_AVG':'float64',
-                'FLOORSMAX_AVG':'float64',
-                'FLOORSMIN_AVG':'float64',
-                'LANDAREA_AVG':'float64',
-                'LIVINGAPARTMENTS_AVG':'float64',
-                'LIVINGAREA_AVG':'float64',
-                'NONLIVINGAPARTMENTS_AVG':'float64',
-                'NONLIVINGAREA_AVG':'float64',
-                'APARTMENTS_MODE':'float64',
-                'BASEMENTAREA_MODE':'float64',
-                'YEARS_BEGINEXPLUATATION_MODE':'float64',
-                'YEARS_BUILD_MODE':'float64',
-                'COMMONAREA_MODE':'float64',
-                'ELEVATORS_MODE':'float64',
-                'ENTRANCES_MODE':'float64',
-                'FLOORSMAX_MODE':'float64',
-                'FLOORSMIN_MODE':'float64',
-                'LANDAREA_MODE':'float64',
-                'LIVINGAPARTMENTS_MODE':'float64',
-                'LIVINGAREA_MODE':'float64',
-                'NONLIVINGAPARTMENTS_MODE':'float64',
-                'NONLIVINGAREA_MODE':'float64',
-                'APARTMENTS_MEDI':'float64',
-                'BASEMENTAREA_MEDI':'float64',
-                'YEARS_BEGINEXPLUATATION_MEDI':'float64',
-                'YEARS_BUILD_MEDI':'float64',
-                'COMMONAREA_MEDI':'float64',
-                'ELEVATORS_MEDI':'float64',
-                'ENTRANCES_MEDI':'float64',
-                'FLOORSMAX_MEDI':'float64',
-                'FLOORSMIN_MEDI':'float64',
-                'LANDAREA_MEDI':'float64',
-                'LIVINGAPARTMENTS_MEDI':'float64',
-                'LIVINGAREA_MEDI':'float64',
-                'NONLIVINGAPARTMENTS_MEDI':'float64',
-                'NONLIVINGAREA_MEDI':'float64',
-                'FONDKAPREMONT_MODE':'object',
-                'HOUSETYPE_MODE':'object',
-                'TOTALAREA_MODE':'float64',
-                'WALLSMATERIAL_MODE':'object',
-                'EMERGENCYSTATE_MODE':'object',
-                'OBS_30_CNT_SOCIAL_CIRCLE':'float64',
-                'DEF_30_CNT_SOCIAL_CIRCLE':'float64',
-                'OBS_60_CNT_SOCIAL_CIRCLE':'float64',
-                'DEF_60_CNT_SOCIAL_CIRCLE':'float64',
-                'DAYS_LAST_PHONE_CHANGE':'float64',
+                'FLAG_CONT_MOBILE':'object',
                 'FLAG_DOCUMENT_2':'object',
                 'FLAG_DOCUMENT_3':'object',
                 'FLAG_DOCUMENT_4':'object',
@@ -341,59 +292,115 @@ df = pd.read_table(
                 'FLAG_DOCUMENT_19':'object',
                 'FLAG_DOCUMENT_20':'object',
                 'FLAG_DOCUMENT_21':'object',
-                'AMT_REQ_CREDIT_BUREAU_HOUR':'float64',
-                'AMT_REQ_CREDIT_BUREAU_DAY':'float64',
-                'AMT_REQ_CREDIT_BUREAU_WEEK':'float64',
-                'AMT_REQ_CREDIT_BUREAU_MON':'float64',
-                'AMT_REQ_CREDIT_BUREAU_QRT':'float64',
-                'AMT_REQ_CREDIT_BUREAU_YEAR':'float64'
+                'FLAG_EMAIL':'object',
+                'FLAG_EMP_PHONE':'object',
+                'FLAG_MOBIL':'object',
+                'FLAG_OWN_CAR':'object',
+                'FLAG_OWN_REALTY':'object',
+                'FLAG_PHONE':'object',
+                'FLAG_WORK_PHONE':'object',
+                'FLOORSMAX_AVG':'float64',
+                'FLOORSMAX_MEDI':'float64',
+                'FLOORSMAX_MODE':'float64',
+                'FLOORSMIN_AVG':'float64',
+                'FLOORSMIN_MEDI':'float64',
+                'FLOORSMIN_MODE':'float64',
+                'FONDKAPREMONT_MODE':'object',
+                'HOUR_APPR_PROCESS_START':'float64',
+                'HOUSETYPE_MODE':'object',
+                'LANDAREA_AVG':'float64',
+                'LANDAREA_MEDI':'float64',
+                'LANDAREA_MODE':'float64',
+                'LIVE_CITY_NOT_WORK_CITY':'object',
+                'LIVE_REGION_NOT_WORK_REGION':'object',
+                'LIVINGAPARTMENTS_AVG':'float64',
+                'LIVINGAPARTMENTS_MEDI':'float64',
+                'LIVINGAPARTMENTS_MODE':'float64',
+                'LIVINGAREA_AVG':'float64',
+                'LIVINGAREA_MEDI':'float64',
+                'LIVINGAREA_MODE':'float64',
+                'NAME_CONTRACT_TYPE':'object',
+                'NAME_EDUCATION_TYPE':'object',
+                'NAME_FAMILY_STATUS':'object',
+                'NAME_HOUSING_TYPE':'object',
+                'NAME_INCOME_TYPE':'object',
+                'NAME_TYPE_SUITE':'object',
+                'NONLIVINGAPARTMENTS_AVG':'float64',
+                'NONLIVINGAPARTMENTS_MEDI':'float64',
+                'NONLIVINGAPARTMENTS_MODE':'float64',
+                'NONLIVINGAREA_AVG':'float64',
+                'NONLIVINGAREA_MEDI':'float64',
+                'NONLIVINGAREA_MODE':'float64',
+                'OBS_30_CNT_SOCIAL_CIRCLE':'float64',
+                'OBS_60_CNT_SOCIAL_CIRCLE':'float64',
+                'OCCUPATION_TYPE':'object',
+                'ORGANIZATION_TYPE':'object',
+                'OWN_CAR_AGE':'float64',
+                'REG_CITY_NOT_LIVE_CITY':'object',
+                'REG_CITY_NOT_WORK_CITY':'object',
+                'REG_REGION_NOT_LIVE_REGION':'object',
+                'REG_REGION_NOT_WORK_REGION':'object',
+                'REGION_POPULATION_RELATIVE':'float64',
+                'REGION_RATING_CLIENT':'object',
+                'REGION_RATING_CLIENT_W_CITY':'object',
+                'SK_ID_CURR':'object',
+                'TARGET':'object',
+                'TOTALAREA_MODE':'float64',
+                'WALLSMATERIAL_MODE':'object',
+                'WEEKDAY_APPR_PROCESS_START':'object',
+                'YEARS_BEGINEXPLUATATION_AVG':'float64',
+                'YEARS_BEGINEXPLUATATION_MEDI':'float64',
+                'YEARS_BEGINEXPLUATATION_MODE':'float64',
+                'YEARS_BUILD_AVG':'float64',
+                'YEARS_BUILD_MEDI':'float64',
+                'YEARS_BUILD_MODE':'float64'
                 }
         )
 
 
-# In[11]:
+# In[ ]:
 
 
 #application_test = pd.read_table(data_file_path + 'application_test.csv', sep=',')
 
 
-# In[12]:
+# In[ ]:
 
 
 #application_train = pd.read_table(data_file_path + 'application_train.csv', sep=',')
 
 
-# In[13]:
+# In[ ]:
 
 
 #bureau = pd.read_table(data_file_path + 'bureau.csv', sep=',')
 
 
-# In[14]:
+# In[ ]:
 
 
 #bureau_balance = pd.read_table(data_file_path + 'bureau_balance.csv', sep=',')
 
 
-# In[15]:
+# In[ ]:
 
 
 #credit_card_balance = pd.read_table(data_file_path + 'credit_card_balance.csv', sep=',')
 
 
-# In[16]:
+# In[ ]:
 
 
 #installments_payments = pd.read_table(data_file_path + 'installments_payments.csv', sep=',')
 
 
-# In[17]:
+# In[ ]:
 
 
 #POS_CASH_balance = pd.read_table(data_file_path + 'POS_CASH_balance.csv', sep=',')
 
 
-# In[18]:
+# In[ ]:
 
 
 #previous_application = pd.read_table(data_file_path + 'previous_application.csv', sep=',')
